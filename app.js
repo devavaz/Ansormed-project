cc07695cefaf2f0461fe70020a41cd49ef2dbfa4
devavaz <135300758+devavaz@users.noreply.github.com>
2023-11-10 01:57:20 +0900

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var scrollToTopButton = document.getElementById("scrollToTop");
     window.onscroll = function() {
       if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         scrollToTopButton.style.display = "block";
+      } else {
         scrollToTopButton.style.display = "none";
       }
     };
