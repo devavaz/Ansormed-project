1f755ce6f1d0abd2907d2a91b618f7b3b015acc3
devavaz <135300758+devavaz@users.noreply.github.com>
2023-11-11 14:31:14 +0900

    New updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var scrollToTopButton = document.getElementById("scrollToTop");
 
-    // Show the button when the user scrolls down 20px from the top
+   
     window.onscroll = function() {
       if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         scrollToTopButton.style.display = "block";
@@ -9,8 +9,8 @@ var scrollToTopButton = document.getElementById("scrollToTop");
       }
     };
 
-    // Scroll to the top when the button is clicked
+    
     scrollToTopButton.addEventListener("click", function() {
-      document.body.scrollTop = 0; // For Safari
-      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+      document.body.scrollTop = 0; 
+      document.documentElement.scrollTop = 0; 
     });
